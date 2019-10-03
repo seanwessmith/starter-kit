d1d536f3dc2693fbe7619d793192f3c5d3c35a0c
Sean Smith <sean.smith@paladin.insure>
2019-10-03 14:02:37 -0700

    removed extra styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React, { Fragment } from 'react';
-import Favicon from 'react-favicon';
 import StarterRouter from './routes/index';
-import icon from './images/favicon.png';
 import './index.scss';
 
 const App = () => (
   <Fragment>
-    <Favicon url={icon} />
     <StarterRouter />
   </Fragment>
 );
